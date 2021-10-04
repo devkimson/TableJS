8746999c5d8e0306ec66e506d03d571b8af54c4a
kkn1125 <xpfjrnseks1@naver.com>
2021-10-04 16:07:54 +0900

    update css and add readonly-btn

--- sort.js
+++ sort.js
@@ -1,4 +1,21 @@
 'use strict';
+
+Date.prototype["format"] = function(str){
+    let time = this;
+    
+    let y = time.getFullYear().toString();
+    let M = (time.getMonth()+1).toString();
+    let d = time.getDate().toString();
+    let H = time.getHours().toString();
+    let m = time.getMinutes().toString();
+    let s = time.getSeconds().toString();
+    let S = time.getMilliseconds().toString();
+
+    return str.replace(/[yMdHmsS]+/gm, x=>{
+        return eval(x.charAt()).slice(-x.length);
+    });
+}
+
 const ks = {};
 
 if(!ks.sort) ks.sort = {};
@@ -13,8 +30,10 @@ if(!ks.sort.editor) ks.sort.editor = (function(){
             uiElem = ui;
             
             uiElem.input.addEventListener('keydown', this.addRow);
+            uiElem.readonly.addEventListener('click', moduleModel.isReadonly);
             uiElem.tbody.addEventListener('click', this.deleteRow);
-            uiElem.sortBtnsWrap.addEventListener('click', this.btnHandler)
+            uiElem.sortBtnsWrap.addEventListener('click', this.btnHandler);
+            uiElem.search.addEventListener('keyup', this.searchByWords);
         }
 
         this.addRow = function(ev){
@@ -24,27 +43,40 @@ if(!ks.sort.editor) ks.sort.editor = (function(){
         this.deleteRow = function(ev){
             let target = ev.target;
 
-            if(target.tagName !== 'SPAN' || target.className !== 'del-btn') return;
+            if(target.tagName !== 'SPAN' || target.className.indexOf('del-btn')==-1) return;
             ev.preventDefault();
 
-            let prt = target.closest('[data-ks-sort]').querySelector('[data-ks-sort-idx]');
+            let wrap = target.closest('[data-ks-sort]');
+            let prt = wrap.querySelector('[data-ks-sort-idx]');
+            wrap.classList.add("del");
 
-            moduleModel.deleteRow({idx:prt.dataset.ksSortIdx, text:prt.nextElementSibling.innerText});
+            setTimeout(()=>{
+                moduleModel.deleteRow({idx:prt.dataset.ksSortIdx, text:prt.nextElementSibling.innerText});
+            }, 500);
         }
 
         this.btnHandler = function(ev){
             moduleModel.btnHandler(ev);
+        }
+
+        this.searchByWords = function(ev){
+            moduleModel.searchByWords(ev);
         }
     }
 
     function Model(){
         let moduleView = null;
         let rows = null;
+        let readonly = null;
 
         this.init = function(view){
             moduleView = view;
             this.getRowsFromLocalStorage();
             this.updateTable();
+        }
+
+        this.searchByWords = function(ev){
+            moduleView.searchByWords(ev, rows);
         }
 
         this.getRowsFromLocalStorage = function(){
@@ -58,6 +90,11 @@ if(!ks.sort.editor) ks.sort.editor = (function(){
         this.updateTable = function(){
             moduleView.updateTable(rows);
             this.setRowsToLocalStorage();
+        }
+
+        this.isReadonly = function(){
+            readonly = !readonly;
+            moduleView.isReadonly(readonly);
         }
         
         this.addRow = function(ev){
@@ -79,7 +116,6 @@ if(!ks.sort.editor) ks.sort.editor = (function(){
         }
 
         this.deleteRow = function(row){
-            
             rows = rows.filter(r=>r.idx!=row.idx);
             this.updateTable();
         }
@@ -94,6 +130,13 @@ if(!ks.sort.editor) ks.sort.editor = (function(){
 
             ev.preventDefault();
             eval(`this.align${text}(rows);`);
+        }
+
+        this.alignClear = function(){
+            if(confirm("Delete the entire row.\nDeleted rows cannot be recovered.")){
+                rows = rows.filter(r=> false);
+                this.updateTable();
+            }
         }
 
         this.alignSort = function(rows){
@@ -130,24 +173,46 @@ if(!ks.sort.editor) ks.sort.editor = (function(){
             uiElem = ui;
         }
 
+        this.searchByWords = function(ev, rows){
+            let value = ev.target.value;
+            let search = rows;
+            if(value.length>0){
+                search = rows.filter(x=>x.row.indexOf(value)>-1);
+            }
+            this.updateTable(search);
+        }
+
         this.updateTable = function(rows){
             let reverse = false;
             let formedRows = '';
             let id = 1;
+
             if(rows.length>0 && rows[0].idx>rows[rows.length-1].idx){
                 reverse = true;
                 id = rows.length;
             }
+
             let rowForm = row => `
-                    <tr data-ks-sort="tr">
+                    <tr data-ks-sort="tr" ${rows[rows.length-1]==row?"class='select'":""}>
                         <td data-ks-sort="index" data-ks-sort-idx="${row.idx}">${reverse?id--:id++}</td>
                         <td>${row.row}</td>
-                        <td><span class="del-btn">&times;</span></td>
+                        <td data-ks-sort="time">
+                            <time>
+                                ${new Date(row.regdate).format("yyyy-MM-dd HH:mm:ss")}
+                            </time>
+                        </td>
+                        <td><span class="del-btn ks-sort">&times;</span></td>
                     </tr>
             `;
 
             rows.forEach(row=>formedRows += rowForm(row));
             uiElem.tbody.innerHTML = formedRows;
+        }
+
+        this.isReadonly = function(readonly){
+            uiElem.search.disabled = readonly;
+            uiElem.input.disabled = readonly;
+            uiElem.wrap.classList.toggle('readonly');
         }
 
         this.clearInput = function(){
@@ -178,6 +243,15 @@ if(!ks.sort.editor) ks.sort.editor = (function(){
             }
         }
 
+        this.generateTh = function(parent){
+            let max = 0;
+            for(let i of Object.values(attr['column'])){
+                if(i!=undefined)
+                    max = Math.max(max, i.length);
+            }
+            return attr['column'][parent]?attr['column'][parent].map(x=>`<th${attr['column'][parent].length==1 && max!=0?' colspan="'+max+'"':''}>${x}</th>`):`<th colspan="${max}">No Value</th>`;
+        }
+
         this.generateTd = function(parent){
             let max = 0;
             for(let i of Object.values(attr['column'])){
@@ -195,26 +269,26 @@ if(!ks.sort.editor) ks.sort.editor = (function(){
             let hide = document.createElement('button');
 
             Object.assign(sortBtnsWrap,{
-                className: "btns-wrap",
+                className: "ks-sort ks-sort-green btns-wrap",
                 type: "button",
             });
             Object.assign(sort,{
-                className: "btns-sort",
+                className: "ks-sort ks-sort-green btns-sort",
                 innerText: 'sort',
                 type: "button",
             });
             Object.assign(reverse,{
-                className: "btns-reverse",
+                className: "ks-sort ks-sort-green btns-reverse",
                 innerText: 'reverse',
                 type: "button",
             });
             Object.assign(clear,{
-                className: "btns-clear",
+                className: "ks-sort ks-sort-green btns-clear",
                 innerText: 'clear',
                 type: "button",
             });
             Object.assign(hide,{
-                className: "btns-hide",
+                className: "ks-sort ks-sort-green btns-hide",
                 innerText: 'hide',
                 type: "button",
             });
@@ -230,11 +304,11 @@ if(!ks.sort.editor) ks.sort.editor = (function(){
             
 
             Object.assign(search,{
-                className: "search-bar",
+                className: "search-bar ks-sort",
                 type: "text",
             });
             Object.assign(searchBtn,{
-                className: "search-btn",
+                className: "search-btn ks-sort ks-sort-main",
                 innerText: "search",
                 type: "button",
             });
@@ -245,12 +319,11 @@ if(!ks.sort.editor) ks.sort.editor = (function(){
             let dom = new DOMParser();
             let div = uiElem.insertAdjacentElement('beforeBegin', document.createElement('div'));
             let searchWrap = div.cloneNode();
-            
             let elements = dom.parseFromString(`
             <table>
                 <thead data-ks-sort="head">
                     <tr data-ks-sort="tr">
-                        ${this.generateTd('head')}
+                        ${this.generateTh('head')}
                     </tr>
                 </thead>
                 <tbody data-ks-sort="body">
@@ -262,6 +335,12 @@ if(!ks.sort.editor) ks.sort.editor = (function(){
                     <tr data-ks-sort="tr">
                         ${this.generateTd('foot')}
                     </tr>
+                    <tr data-ks-sort="tr">
+                        <td>
+                            <input id="readonly" type="checkbox" class="readonly ks-sort">
+                            <label for="readonly">readonly</label>
+                        </td>
+                    </tr>
                 </tfoot>
             </table>
             `, 'text/html')
@@ -271,12 +350,13 @@ if(!ks.sort.editor) ks.sort.editor = (function(){
 
             div.dataset.ksSort = 'wrap';
             div.append(uiElem);
-            div.append(new DOMParser().parseFromString(`<div>
-            <input type="text" data-ks-sort="input">
+            div.append(new DOMParser().parseFromString(`<div class="input">
+            <input class="ks-sort" type="text" data-ks-sort="input" placeholder="내용 등록">
             </div>`,'text/html').querySelector('div'));
             
             div.prepend(this.createSortBtnsPart());
             searchWrap.prepend(...this.createSeachPart());
+            searchWrap.classList.add("search-wrap");
             div.prepend(searchWrap);
         }
     }
@@ -289,6 +369,7 @@ if(!ks.sort.editor) ks.sort.editor = (function(){
                     head: [
                         "구분",
                         "내용",
+                        "생성시간",
                         "비고"
                     ],
                     foot: [
@@ -299,8 +380,8 @@ if(!ks.sort.editor) ks.sort.editor = (function(){
             if(this.checkValid(attr)){
                 const body = document.body;
                 const wrap = document.querySelector('[data-ks-sort="wrap"]');
-                const search = document.querySelector('.search');
-                const searchBtn = document.querySelector('.searchBtn');
+                const search = document.querySelector('.search-bar');
+                const searchBtn = document.querySelector('.search-btn');
                 const input = document.querySelector('[data-ks-sort="input"]');
                 const tb = document.querySelector('[data-ks-sort="table"]');
                 const thead = document.querySelector('[data-ks-sort="head"]');
@@ -311,6 +392,7 @@ if(!ks.sort.editor) ks.sort.editor = (function(){
                 const reverse = document.querySelector('.btns-reverse');
                 const clear = document.querySelector('.btns-clear');
                 const hide = document.querySelector('.btns-hide');
+                const readonly = document.querySelector('.readonly');
                 
                 const ui = {
                     body,
@@ -327,6 +409,7 @@ if(!ks.sort.editor) ks.sort.editor = (function(){
                     reverse,
                     clear,
                     hide,
+                    readonly,
                 }
     
                 const view = new View();
